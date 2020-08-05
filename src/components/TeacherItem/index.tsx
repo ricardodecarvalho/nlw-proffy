ea333d85597c799ae358055adfbcecfb409851d6
Ricardo de Carvalho <skapitao@gmail.com>
2020-08-05 19:50:33 -0300

    feat: Added new features from lesson 3

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -2,36 +2,53 @@ import React from 'react'
 import whatsAppIcon from "../../assets/images/icons/whatsApp.svg";
 
 import './styles.css'
+import api from '../../services/api';
 
-function TeacherItem() {
+interface TeacherItemProps {
+    teacher: {
+        id: number
+        avatar: string;
+        bio: string;
+        cost: number;
+        name: string;
+        subject: string;
+        whatsapp: string;
+    }
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+
+    function createNewConnection () {
+        api.post('connections', {
+            user_id: teacher.id
+        })
+    }
+
     return (
         <article className="teacher-item">
             <header>
                 <img
-                    src="https://avatars0.githubusercontent.com/u/6460158?s=460&u=0906959bde7441d211f191f943f6a210694ea3d8&v=4"
-                    alt="Ricardo de Carvalho" />
+                    src={teacher.avatar}
+                    alt={teacher.name} />
                 <div>
-                    <strong>Ricardo de Carvalho</strong>
-                    <span>Química</span>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
                 </div>
             </header>
 
             <p>
-                Lorem Ipsum is simply dummy text of the printing.
-                <br /><br />
-                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown
-                printer took a galley of type and scrambled it to make a type specimen book.
+                {teacher.bio}
             </p>
 
             <footer>
                 <p>
                     Preço/hora
-                    <strong>R$ 120,00</strong>
+                    <strong>R$ {teacher.cost}</strong>
                 </p>
-                <button type="button">
+                <a target="_blank" onClick={createNewConnection} href={`https://wa.me/55${teacher.whatsapp}`}>
                     <img src={whatsAppIcon} alt="WhatsApp" />
                     Entrar em contato
-                </button>
+                </a>
             </footer>
         </article>
     )
